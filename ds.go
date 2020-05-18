b7d25a0ddd8b2a5e5ff9247c7a7518520b4e800f
Abhigyan Khaund <20434085+abhigyank@users.noreply.github.com>
2020-05-18 17:11:05 +0530

    Connect node to master.

--- ds.go
+++ ds.go
@@ -23,22 +23,37 @@ func createMaster (port string) {
 		if err != nil {
 			fmt.Println("Error occured in accepting connection: ", err)
 		} else {
-			fmt.Println("Received request to master.")
+			fmt.Println("Received connection to master.")
 			// Code here for add node to cluster
 			conn.Close()
 		}
 	}
 }
 
+func connectToMaster(myPort string, masterPort string) {
+	 _, err := net.Dial("tcp", fmt.Sprint(ipAddress + ":" + masterPort))
+	 if err != nil {
+        fmt.Println("Couldn't connect to master.")
+        os.Exit(0)
+    } else {
+    	fmt.Println("Connection to master successful.")
+    }
+}
+
 func main() {
 	amIMaster := flag.Bool("createMaster", false, "Create this node as master.")
 	myPort :=  flag.String("nodePort", "8001", "Port to run this node on.")
+	masterPort :=  flag.String("masterPort", "8001", "Port on which master is running on, not required for master node.")
 	flag.Parse()
 
 	if (*amIMaster) {
 		fmt.Println("Creating master node...")
 		fmt.Println("Master will listen on port =", *myPort)
 		createMaster(*myPort)
+	} else {
+		fmt.Println("Master is running on port = ", *masterPort)
+		fmt.Println("Trying to connect to master...")
+		connectToMaster(*myPort, *masterPort)
 	}
 
 }
